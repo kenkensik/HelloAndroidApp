b740711136a0cc5c537123adcac8fe6bf09c7cfd
kenkensik <kenkensik@yahoo.co.jp>
2019-10-03 21:06:29 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/kento/saka/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.kento.saka.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
